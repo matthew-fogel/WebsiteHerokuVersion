4666d168b65e9c3a677744f5f4ecea3a5e67f365
Matthew Fogel <matthew-fogel@users.noreply.github.com>
2021-01-27 20:36:23 -0600

    Fix bug

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-pp.use('/static', express.static(path.join(__dirname, 'images')));
+app.use('/static', express.static(path.join(__dirname, 'images')));
 
 app.use('/', indexRouter);
 
