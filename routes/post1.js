0b1e99aeecbb5df802cc90e2e497d52c585994dc
Matthew Fogel <matthew-fogel@users.noreply.github.com>
2021-02-04 16:41:32 -0600

    Refactor posts into single folder

--- routes/post1.js
+++ routes/post1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET post1 page. */
 router.get('/', function(req, res, next) {
-  res.render('post1', { title: 'Matthew Fogel'});
+  res.render('posts/post1', { title: 'Matthew Fogel'});
 });
 
 module.exports = router;
--- routes/post2.js
+++ routes/post2.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET post2 page. */
 router.get('/', function(req, res, next) {
-  res.render('post2', { title: 'Matthew Fogel'});
+  res.render('posts/post2', { title: 'Matthew Fogel'});
 });
 
 module.exports = router;
