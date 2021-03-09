8a5129019132aaacd77d8211d29cccaf898f9434
Matthew Fogel <matthew-fogel@users.noreply.github.com>
2021-03-08 19:46:37 -0600

    Revert "Simplify site"
    This reverts commit 434e389646eb2d52af0094b2e8ac5aded39b84a1.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var blogRouter = require('./routes/blog');
+var blogPostRouter = require('./routes/posts');
 var compression = require('compression');
 var helmet = require('helmet');
 
@@ -43,6 +45,8 @@ app.use('/icons', express.static(path.join(__dirname, 'public/icons')));
 
 
 app.use('/', indexRouter);
+app.use('/blog', blogRouter);
+app.use('/', blogPostRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
